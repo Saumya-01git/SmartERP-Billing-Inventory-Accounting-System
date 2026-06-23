208b4a1e29115651c2213f5d0d9ada1d6a3e5f77
Saumya <saumyagarg55555@gmail.com>
2026-06-23 23:27:18 +0530

    Day 3: Implement user registration, login, password hashing and JWT authentication

--- server/server.js
+++ server/server.js
@@ -3,11 +3,15 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const pool = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
+
+
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("SmartERP Backend Running");
